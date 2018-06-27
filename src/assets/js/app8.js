c7b61bb328663e994bff8ac1b1cc2b5a38336f8f
Jacek Seń <jaceksen@gmail.com>
2018-06-27 21:26:44 +0200

    ddd

---
+++ src/assets/js/app8.js
@@ -0,0 +1,34 @@
+// Window Object
+
+// console.log('App8');
+// const input = prompt();
+// alert(input);
+
+// if(confirm('Are you sure')){
+//     console.log('YES');
+// } else {
+//     console.log('NO');
+// }
+
+//height width of the window
+val = window.outerHeight;
+val2 = window.outerWidth;
+val = window.innerHeight;
+val2 = window.innerWidth;
+console.log(val);
+console.log(val2);
+
+//scroll points
+val3 = window.scrollY;
+console.log(val3);
+
+//location
+val4 = window.location.hostname;
+// window.location.href='http://google.com'
+
+console.log(val4);
+
+console.log(window.history.length);
+
+val5 = window.navigator;
+console.log(val5);
