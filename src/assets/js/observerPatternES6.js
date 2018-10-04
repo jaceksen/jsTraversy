10c1091e20675b1082e00061769f2e172011c0e8
Jacek Seń <jaceksen@gmail.com>
2018-10-04 19:16:09 +0200

    JS Patterns

--- src/assets/js/observerPatternES6.js
+++ src/assets/js/observerPatternES6.js
@@ -1,25 +1,24 @@
-//constructor function
-function EventObserver() {
-  this.observers = [];
-}
 
-EventObserver.prototype = {
-  subscribe: function(fn) {
+class EventObserver {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(fn) {
     this.observers.push(fn);
     console.log(`You are now subscribed to ${fn.name}`);
-  },
-  unsubscribe: function(fn) {
-    // Filter out from the list whatever matches the callback
-    // function. It there is no match, the callback gets to stay on the list.
-    // The filter returns a new list and reassigns the lost of observers.
+  }
+
+  unsubscribe(fn) {
     this.observers = this.observers.filter(function(item) {
       if(item !== fn) {
         return item;
       }
     });
     console.log(`You are now unusubscirbed from ${fn.name}`);
-  },
-  fire: function() {
+  }
+
+  fire() {
     this.observers.forEach(function(item) {
       item.call();
     });
